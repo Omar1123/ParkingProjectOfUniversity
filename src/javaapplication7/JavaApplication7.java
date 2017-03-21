80a87c1049c1f1110bd47ebdffefafcc31ad807a
Omar1123 <omar1123veliz@gmail.com>
2017-03-20 21:39:20 -0600

    Add new views for the project

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author jake
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.getContentPane().add(new ParqueosUMG());
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
