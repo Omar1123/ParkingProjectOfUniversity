0c3ba22a43ef34d91f9c371ef8bab73d28c63207
Omar1123 <omar1123veliz@gmail.com>
2017-03-22 20:17:02 -0600

    ADD: new methods for the handler

--- src/javaapplication7/Handler/ParkingsHandler.java
+++ src/javaapplication7/Handler/ParkingsHandler.java
@@ -5,6 +5,8 @@
  */
 package javaapplication7.Handler;
 
+import java.util.Random;
+
 /**
  *
  * @author jake
@@ -12,8 +14,10 @@ package javaapplication7.Handler;
 public class ParkingsHandler {
     
     private static ParkingsHandler instance;
+    public Random random = new Random();
     
     private ParkingsHandler() {
+    
     }
     
     public static ParkingsHandler getInstancia() {
@@ -24,4 +28,20 @@ public class ParkingsHandler {
         
         return instance;
     }
+    
+    private void newParking(String idParking, String username, int basement, 
+            int parking, String hour, String date) {
+                        
+    }
+    
+    private String generateParkingId() {
+        
+        random = new Random();
+        
+        return random.toString();
+    }
+    
+    
+    
+    
 }
